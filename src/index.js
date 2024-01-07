169506e1f652e45688b3f36bb954f547956c16d0
Maksim Mamontov <80010074+MrMasimo@users.noreply.github.com>
2024-01-07 19:13:58 +0300

    array object

--- src/index.js
+++ src/index.js
@@ -1,11 +1,36 @@
-import './style.css';
+import "./style.css";
 
-const f = (num) => {
-    if (num === 10) {
-        console.log('Ровно 10');
-    } else {
-        console.log('Не ровно 10');
-    }
-};
+// Task #1
 
-f();
+const prices = [100, 150, 200, 250];
+
+let pricesCurr = prices.map((price) => price + " р");
+console.log(pricesCurr);
+
+// Task #2
+
+const users = [
+    ["alex", 32],
+    ["tomas", 17],
+    ["olga", 14],
+    ["andre", 24]
+];
+let usersUnder18 = users.filter((user) => {
+    if (user[1] > 18) return true;
+});
+console.log(usersUnder18);
+
+// Task #3
+
+const items = [
+    { title: "пицца", price: 200 },
+    { title: "баранина", price: 300 },
+    { title: "креветки", price: 400 }
+];
+
+let sum = items.reduce((acc, item) => {
+    acc += item.price;
+    return acc;
+}, 0);
+
+console.log(sum);
