509c7d73b73d1c1802e028cb1d7412dd45c291a8
Maksim Mamontov <80010074+MrMasimo@users.noreply.github.com>
2024-01-27 20:23:51 +0300

    typescript

--- src/index.js
+++ src/index.js
@@ -1,36 +1,25 @@
-import "./style.css";
-
-// Task #1
-
-const prices = [100, 150, 200, 250];
-
-let pricesCurr = prices.map((price) => price + " р");
+var prices = [100, 150, 200, 250];
+var pricesCurr = prices.map(function (price) { return price + " р"; });
 console.log(pricesCurr);
-
 // Task #2
-
-const users = [
+var users = [
     ["alex", 32],
     ["tomas", 17],
     ["olga", 14],
     ["andre", 24]
 ];
-let usersUnder18 = users.filter((user) => {
-    if (user[1] > 18) return true;
+var usersUnder18 = users.filter(function (user) {
+    if (user[1] > 18)
+        return true;
 });
 console.log(usersUnder18);
-
-// Task #3
-
-const items = [
+var item = [
     { title: "пицца", price: 200 },
     { title: "баранина", price: 300 },
     { title: "креветки", price: 400 }
 ];
-
-let sum = items.reduce((acc, item) => {
+var sum = item.reduce(function (acc, item) {
     acc += item.price;
     return acc;
 }, 0);
-
 console.log(sum);
